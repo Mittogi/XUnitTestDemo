06542bafc002f9696d16db54238c42e2565fbff5
Vo Thanh Phat <vophat54321@gmail.com>
2024-06-11 14:37:13 +0700

    First commit for xunit test framework

---
+++ PrimaryNumber/PrimaryNumber.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathHelper
+{
+    public class PrimaryNumber
+    {
+        public static bool CheckIsPrimaryNumber(int number)
+        {
+            if (number < 2)
+            {
+                return false;
+            }
+
+            if (number == 2)
+            {
+                return true;
+            }
+
+            for (int i = 2; i < Math.Sqrt(number); i++)
+            {
+                if (number % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
